4d36bab1deb93c8afcfa4c81b4a6f9457b29731f
kello22e <kello22e@mtholyoke.edu>
2018-06-04 23:42:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ class Song
     
     song = self.create
     song.artist_name = artist
-    song.name = name_of_song
+    song.name = name_of_song[0]
     song
   end 
   
@@ -50,7 +50,7 @@ class Song
     
     song = self.new
     song.artist_name = artist
-    song.name = name_of_song
+    song.name = name_of_song[0]
     song
   end 
   
